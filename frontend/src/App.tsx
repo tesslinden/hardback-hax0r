9e1ebc862330e9e65af47f12ac68b57c38c540c7
Tess Linden <tlinden@bighatbio.com>
2024-11-16 17:23:15 -0800

    display backend response on webpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const App: React.FC = () => {
+
+  const [data, setData] = useState('');
+  // ^ creates a state variable `data` and a function `setData` to update it. initializes it as an empty string.
+
   useEffect(() => {
     axios.get('http://127.0.0.1:5000/')
       .then(response => {
         console.log('Success:', response.data); // response.data = 'Hello, World!'
+        setData(response.data);
       })
       .catch(error => {
         console.error('There was an error!', error);
@@ -15,8 +20,10 @@ const App: React.FC = () => {
   return (
     <div>
       <h1>My React App</h1>
+        <p>{data}</p> {/* display response.data (Hello, World!) */}
     </div>
   );
+
 };
 
 export default App;
