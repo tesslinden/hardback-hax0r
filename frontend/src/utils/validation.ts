f8590d5dc735901c4b2a773182ed842ab8738dcc
Tess Linden <tlinden@bighatbio.com>
2024-12-30 11:23:39 -0800

    add isValidUUID util

---
+++ frontend/src/utils/validation.ts
@@ -0,0 +1,5 @@
+export const isValidUUID = (uuid: string): boolean => {
+  const uuidV4Regex =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  return uuidV4Regex.test(uuid);
+};
